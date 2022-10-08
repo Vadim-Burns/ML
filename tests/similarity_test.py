88d3dd98ef5eb5c5be3bf48acbe9e8b722ba79ac
Ivan <irodkin2002@gmail.com>
2022-10-08 16:22:08 +0300

    add kwords correspondance test

--- tests/similarity_test.py
+++ tests/similarity_test.py
@@ -14,12 +14,28 @@ texts = ['Люди совершают самоубийства чаще, чем 
          'Зеленский призвал НАТО к превентивному удару по России'
          ]
 
+kwords = ['Война оружие запад', 'Смерть статистика']
+
 def print_similarity(text_1, text_2):
 
     sm = model.similarity(text_1, text_2)
     print('='*60)
     print(f'similarity between: \n {text_1} \n and \n {text_2}: \n {sm}')
 
+def print_kwords_sim(text, kwords):
+    sm = model.compare_text_with_kwords(text, kwords)
+
+    print('='*60)
+    print(f'Correspondance of: \n {kwords} \n to \n {text}: {sm}')
+
 for i in range(len(texts)):
     for j in range(i+1, len(texts)):
         print_similarity(texts[i], texts[j])
+
+print('='*89)
+print('-'*89)
+print('='*89)
+
+for i in range(len(texts)):
+    for j in range(len(kwords)):
+        print_kwords_sim(texts[i], kwords[j])
